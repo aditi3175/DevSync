f69a0a96fd26f99fea46e468538aecccf17f47c2
aditi3175 <aditigupta3175@gmail.com>
2025-12-14 20:08:32 +0530

    Frontend Updated

--- devsync-frontend/vite.config.js
+++ devsync-frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-// Vite configuration for DevSync Frontend
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
